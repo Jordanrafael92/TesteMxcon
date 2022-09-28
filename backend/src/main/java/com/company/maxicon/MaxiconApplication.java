5a0a6ecc8f7d8d378f673a3ecf67f0f5d6fdecee
Jordan <jordanrafael92@gmail.com>
2022-09-28 12:23:40 -0300

    Projeto criado - desafio

---
+++ backend/src/main/java/com/company/maxicon/MaxiconApplication.java
@@ -0,0 +1,13 @@
+package com.company.maxicon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxiconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxiconApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/company/maxicon/MaxiconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.maxicon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxiconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
